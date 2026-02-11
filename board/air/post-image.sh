d2b6fec70efcafad52f3b5a1cd78cc26b8980e5b
Mx7mm <justin.koehler.f@icloud.com>
2026-02-11 01:08:22 +0100

    Allow GRUB modules from target

--- board/air/post-image.sh
+++ board/air/post-image.sh
@@ -24,17 +24,30 @@ echo "HOST_DIR: ${HOST_DIR}"
 # Create EFI boot directory structure
 mkdir -p "${BINARIES_DIR}/EFI/BOOT"
 
-# Check if GRUB modules exist
-GRUB_MODULES_DIR="${HOST_DIR}/lib/grub/x86_64-efi"
-if [ ! -d "$GRUB_MODULES_DIR" ]; then
-    echo "ERROR: GRUB modules not found at $GRUB_MODULES_DIR"
+# Check if GRUB tools and modules exist
+GRUB_MKIMAGE="${HOST_DIR}/bin/grub-mkimage"
+if [ ! -x "$GRUB_MKIMAGE" ]; then
+    echo "ERROR: grub-mkimage not found at $GRUB_MKIMAGE"
+    exit 1
+fi
+
+GRUB_MODULES_DIR_HOST="${HOST_DIR}/lib/grub/x86_64-efi"
+GRUB_MODULES_DIR_TARGET="${TARGET_DIR}/usr/lib/grub/x86_64-efi"
+if [ -d "$GRUB_MODULES_DIR_HOST" ]; then
+    GRUB_MODULES_DIR="$GRUB_MODULES_DIR_HOST"
+elif [ -d "$GRUB_MODULES_DIR_TARGET" ]; then
+    GRUB_MODULES_DIR="$GRUB_MODULES_DIR_TARGET"
+else
+    echo "ERROR: GRUB modules not found in host or target"
+    echo "Tried: $GRUB_MODULES_DIR_HOST"
+    echo "Tried: $GRUB_MODULES_DIR_TARGET"
     exit 1
 fi
 
 echo "Creating GRUB EFI binary..."
 
 # Create GRUB EFI binary using grub-mkimage
-"${HOST_DIR}/bin/grub-mkimage" \
+"$GRUB_MKIMAGE" \
     -O x86_64-efi \
     -o "${BINARIES_DIR}/EFI/BOOT/bootx64.efi" \
     -p /EFI/BOOT \
